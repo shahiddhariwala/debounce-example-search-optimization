b2e4c2faa6d4e61f9e337d9fc55b02f8803f476a
Shahid Dhariwala <32133607+shahiddhariwala@users.noreply.github.com>
2021-09-11 22:53:07 +0530

    Added debounce function

--- src/App.js
+++ src/App.js
@@ -7,16 +7,31 @@ const InputText = styled.input`
   height: 20px;
   width: 100%;
 `;
+
+const debounce = (func, timeout = 1000) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+};
 export default function App() {
   const [searchVal, setSearchVal] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  const addDebouncedSearchVal = debounce(setSearchVal);
 
   const onChangeHandler = (event) => {
-    setSearchVal(event.target.value);
+    let val = event.target.value;
+    setInputValue(val);
+    addDebouncedSearchVal(val);
   };
   return (
     <div className="App">
       <label>Search Box</label>
-      <InputText onChange={onChangeHandler} value={searchVal} />
+      <InputText onChange={onChangeHandler} value={inputValue} />
       <p>
         <span>Searched : </span>
         {searchVal}
